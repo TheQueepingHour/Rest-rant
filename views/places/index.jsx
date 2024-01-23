207a4d49ee7ab73f096170cd4405949192465da9
Josh Rivera <beps@Mactop.local>
2024-01-23 16:06:53 -0600

    Added new places folder within views folder, created index.jsx for places index, added shell HTML to index.jsx, and changed places.js to render index.jsx

---
+++ views/places/index.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+const Def = require('../default')
+
+function index() {
+    return(
+        <Def>
+            <main>
+                <h1>Index Page</h1>
+                <p>Welcome to the index.</p>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = index
