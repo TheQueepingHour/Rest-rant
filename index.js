8533308e8cfcebf1ca9061f225d24991c2ad3ade
Josh Rivera <beps@Mactop.local>
2024-01-23 15:53:48 -0600

    Added home.jsx and added shell HTML, also changed home page to render home function

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Home')
+    res.render('home')
+    // res.send('Home')
 })
 
 app.get('*', (req, res) => {
