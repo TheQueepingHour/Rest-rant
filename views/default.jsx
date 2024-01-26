ceeee1f085dc95090b075d23af70b650f7dc5314
Josh Rivera <beps@Mactop.local>
2024-01-25 22:07:36 -0600

    Added Bootstrap and added css to default.jsx

--- views/default.jsx
+++ views/default.jsx
@@ -5,9 +5,14 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"></link>
+                <link href="/css/style.css" rel="stylesheet"/>
             </head>
-            <body>
+            <body id="defaultBody">
                 {html.children}
+
+                {/* Bootstrap Script */}
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
             </body>
         </html>
     )
