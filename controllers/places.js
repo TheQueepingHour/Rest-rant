8cba0359f65899ab7a8fc78b2c1951264349b75f
Josh Rivera <beps@Mactop.local>
2024-01-26 18:38:48 -0600

    Added new images for placeholder restaraunts

--- controllers/places.js
+++ controllers/places.js
@@ -7,14 +7,14 @@ router.get('/', (req, res) => {
             city: 'Chicago',
             state: 'IL',
             cuisines: 'Pizza, Hot dogs',
-            pic: 'http://placekitten.com/250/250'
+            pic: '/images/concrete-building.jpg'
         },
         {
             name: 'Coding Cat Cafe',
             city: 'Pheonix',
             state: 'AZ',
             cuisines: 'Coffee, Bakery',
-            pic: 'http://placekitten.com/250/250'
+            pic: '/images/cafe-ambiance.jpg'
         }
     ]
     res.render('places/index', {places})
