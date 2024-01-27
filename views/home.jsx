b2b7ec061b6d74a06a616bce8180aecdf88b2358
Josh Rivera <beps@Mactop.local>
2024-01-27 09:32:29 -0600

    Added carousel and extra detail for home page

--- views/home.jsx
+++ views/home.jsx
@@ -12,15 +12,30 @@ function home() {
                         <button className="btn-primary p-2">Places Page</button>
                     </a>
                 </div>
-                
+                <div className="carousel slide" id="homeCarousel">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img src="/images/dish-white-plate.jpg" alt="Dish on white ceramic plate" className="d-block w-100" id="carouselImgs"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img src="/images/greenery-bar.jpg" alt="modern bar with overhead greenery" className="d-block w-100" id="carouselImgs"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img src="/images/stone-cafe.jpg" alt="Old stone cafe building underneath blue sky" className="d-block w-100" id="carouselImgs"/>
+                        </div>
+                    </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#homeCarousel" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#homeCarousel" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
             </main>
         </Def>
     )
 }
-{/* 
-<div>
-                    <img src="/images/dish-white-plate.jpg" alt="Dish on white ceramic plate" id="homePic"/>
-                        Photo by <a href="https://unsplash.com/@jaywennington">Jay Wennington</a> on <a href="https://unsplash.com">UnSplash</a>
-                    </div>
-*/}
+
 module.exports = home
