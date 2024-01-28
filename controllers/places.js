ac6e36b6976b94cc47ade05001177cb96b692d39
Josh Rivera <beps@Mactop.local>
2024-01-28 11:46:16 -0600

    fixed error by moving new.jsx into places folder because I can't read. Also added route for new.jsx

--- controllers/places.js
+++ controllers/places.js
@@ -20,4 +20,12 @@ router.get('/', (req, res) => {
     res.render('places/index', {places})
 })
 
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
+router.post('/', (req, res) => {
+    res.send('POST /places stub')
+})
+
 module.exports = router
--- views/error404.jsx
+++ views/error404.jsx
@@ -6,13 +6,13 @@ function error404() {
         <Def>
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
-                <div>
+                <div className="d-flex flex-column align-items-center">
                     <img src="/images/funny-monkey.jpg" alt="Monkey holding handrail" id="monkey404"/>
                     <div>
                         Photo by <a href="https://unsplash.com/@jareddrice">Jared Rice</a> on <a href="https://unsplash.com">UnSplash</a>
                     </div>
                 </div>
-                <p>Sorry, can't find this page!</p>
+                <p className="text-center fs-5">Sorry, can't find this page!</p>
             </main>
         </Def>
     )
