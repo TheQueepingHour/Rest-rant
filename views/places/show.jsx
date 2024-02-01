98357b375edf5d1750578ba404bc5f1145310de7
Josh Rivera <beps@Mactop.local>
2024-02-01 17:44:27 -0600

    Started work on filling out/stylizing show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,26 @@
 const React = require('react')
 const Def = require('../default')
 
-function show() {
+function show(data) {
     return (
         <Def>
-            <main>
-                <h1>Show Page</h1>
+            <main className='d-flex'>
+                <img src={data.place.pic} alt={data.place.name} className='indexImgs'/>
+                <div className='d-flex flex-column align-items-center'>
+                    <h1 className='text-center'>{data.place.name}</h1>
+                    <div>
+                        <h2>Rating</h2>
+                        <p>Not rated</p>
+                    </div>
+                    <div>
+                        <h2>Description</h2>
+                        <p>Located in {data.place.city}, {data.place.state}, and serving {data.place.cuisines}</p>
+                    </div>
+                </div>
+                <div>
+                    <h2>Comments</h2>
+                    <p>No comments yet!</p>
+                </div>
             </main>
         </Def>
     )
