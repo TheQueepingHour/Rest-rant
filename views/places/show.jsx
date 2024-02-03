e7246759d88b25c043593da9c74f4185535bfb9f
Josh Rivera <beps@Mactop.local>
2024-02-02 19:53:13 -0600

    Added formatting for show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -4,9 +4,9 @@ const Def = require('../default')
 function show(data) {
     return (
         <Def>
-            <main className='d-flex'>
-                <img src={data.place.pic} alt={data.place.name} className='indexImgs'/>
-                <div className='d-flex flex-column align-items-center'>
+            <main className='d-flex justify-content-evenly'>
+                <img src={data.place.pic} alt={data.place.name} className='indexImgs p-3'/>
+                <div className='d-flex flex-column align-items-center p-5'>
                     <h1 className='text-center'>{data.place.name}</h1>
                     <div>
                         <h2>Rating</h2>
@@ -17,11 +17,11 @@ function show(data) {
                         <p>Located in {data.place.city}, {data.place.state}, and serving {data.place.cuisines}</p>
                     </div>
                 </div>
-                <div>
+            </main>
+            <div className='d-flex flex-column align-items-center mb-5'>
                     <h2>Comments</h2>
                     <p>No comments yet!</p>
-                </div>
-            </main>
+            </div>
         </Def>
     )
 }
